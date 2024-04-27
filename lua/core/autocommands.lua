7983348e187db6fb1767ad6c1fee74975473588a
Antti Sivula <antti.j.sivula@gmail.com>
2024-04-27 10:42:41 +0300

    fix gofmt autocommand

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
+vim.api.nvim_create_autocmd("BufWritePost", {
   group = vim.api.nvim_create_augroup("go fmt", { clear = true }),
   pattern = "*.go",
   callback = function()
@@ -11,9 +11,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         end
       end
     })
+
     buffer_text = vim.api.nvim_buf_get_lines(0, 0, -1, false)
     vim.api.nvim_chan_send(job_id, table.concat(buffer_text, "\n"))
     vim.fn.chanclose(job_id, "stdin")
+
     vim.fn.jobwait({job_id})
+    vim.cmd(":w")
   end
 })
